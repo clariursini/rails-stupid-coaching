e91348697d1a8153dc27756a9ccb5db268342f49
Clari Ursini <clari_ursini@hotmail.com>
2022-08-15 13:48:27 -0300

    Made the app work

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'questions/ask', to: 'questions#ask'
+  get 'questions/answer', to: 'questions#answer'
 end
